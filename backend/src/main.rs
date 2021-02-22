16c80bd781570b6fc309191211da0377b2efad43
hardliner66 <hardliner66@gmail.com>
2021-02-22 15:37:27 +0100

    add test route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,20 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
+#[get("/")]
+fn root() -> String {
+    "It seems you are too early. Maybe come back at a latet time!".to_string()
+}
+
 #[launch]
 fn rocket() -> rocket::Rocket {
     dotenv::dotenv().ok();
-    rocket::ignite().mount("/hello", routes![hello])
+    rocket::ignite()
+        .mount("/", routes![root])
+        .mount("/hello", routes![hello])
 }
