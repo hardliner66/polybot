a83b5f282d492c46236317f7523fc2ba6565a1e3
hardliner66 <hardliner66@gmail.com>
2021-02-22 21:36:34 +0100

    update readme

--- README.md
+++ README.md
@@ -10,12 +10,30 @@ This will basically be my training ground for all languages and technologies.
 - Use many languages and technologies:
     - at least one imperative language
     - at least one functional langauge
-    - at least one relational database
-    - at least one no-sql database
+    - at least one relational database (PostgreSQL)
+    - at least one nosql database (not PostgreSQL)
 - Use modern deployment (docker, kubernetes)
 - Bot features:
     - Plugin System (WASM or script engine)
     - Point System
+- Client:
+    - sqlite
+
+## Database
+- Library:
+    - diesel (i intuitively prefer this one for use behind an API)
+        - not async
+        - orm
+    - sqlx
+        - async
+        - no orm
+        - type checked SQL
+- Database access is managed through an API server
+    - Services communicate with API, API communicates with DB
+- Communication:
+    - https://www.grpc.io/
+        - https://crates.io/crates/tonic
+    - graphql
 
 ## Non-Features
 - I don't want to have every language technologie there is.
@@ -24,4 +42,4 @@ This will basically be my training ground for all languages and technologies.
     - If the project works for you, use it.
     - If it doesn't, I probably wont change it.
     - I try to keep everything as generic and adaptable as possible and I will accept feature and change requests
-      if i think they are a good fit.
+      if I think they are a good fit.
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,57 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::{http::Status, outcome::Outcome, request::FromRequest};
+
+struct ApiKey(String);
+
+#[derive(Debug)]
+enum ApiKeyError {
+    Missing,
+    Invalid,
+    BadCount,
+}
+
+/// Returns true if `key` is a valid API key string.
+fn is_valid(key: &str) -> bool {
+    std::fs::read_to_string("./apikeys.txt")
+        .unwrap_or_default()
+        .lines()
+        .any(|line| key == line)
+}
+
+#[rocket::async_trait]
+impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
+    type Error = ApiKeyError;
+
+    async fn from_request(
+        request: &'a rocket::Request<'r>,
+    ) -> rocket::request::Outcome<Self, Self::Error> {
+        if std::env::args().any(|x| x == "--dev") {
+            return Outcome::Success(ApiKey("".to_string()));
+        }
+        let keys: Vec<_> = request.headers().get("x-api-key").collect();
+        match keys.len() {
+            0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
+            1 if is_valid(keys[0]) => Outcome::Success(ApiKey(keys[0].to_string())),
+            1 => Outcome::Failure((Status::BadRequest, ApiKeyError::Invalid)),
+            _ => Outcome::Failure((Status::BadRequest, ApiKeyError::BadCount)),
+        }
+    }
+}
+
 #[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
+fn hello(_key: ApiKey, name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
 #[get("/")]
 fn root() -> String {
-    "It seems you are too early. Maybe come back at a latet time!".to_string()
+    "It seems you are too early. Maybe come back at a later time!".to_string()
 }
 
 #[launch]
-fn rocket() -> rocket::Rocket {
+fn rocket() -> _ {
     dotenv::dotenv().ok();
     rocket::ignite()
         .mount("/", routes![root])
